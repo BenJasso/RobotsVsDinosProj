ba8dda94498c9c88248efb2dc522536d390170a3
Ben Jasso <benjaminjasso@icloud.com>
2020-03-16 16:46:23 -0500

    newest update

--- DinoVsRobo/AttackPhase.cs
+++ DinoVsRobo/AttackPhase.cs
@@ -45,94 +45,89 @@ namespace DinoVsRobo
                 
                 while (roundDino.health > 0 && roundRobo.health > 0)
                 {
-                    
-                    if (attacker == roundRobo.name && roundRobo.powerLevel > 0)
-                         {
-                            Console.WriteLine($"{roundRobo.name}: Select the number of the weapon you want to attack with or to restore your energy.\n1) Electric Blaster: PWR 50, ENG COST 25\n2) Zapper: PWR 20, ENG COST 10  \n3) Ray Gun: PWR 35, ENG COST 15\n4) Restore Energy: ENG +100\n");
-                            weaponChoice = Console.ReadLine();
-                                if (weaponChoice == "1")
-                                {
-                                    roundRobo.weaponName = gameWeaponList.electricBlaster.name;
-                                    roundRobo.weaponPower = gameWeaponList.electricBlaster.attackPower;
-                                    roundRobo.energyCost = gameWeaponList.electricBlaster.energyCost;
-                                }
-                                else if (weaponChoice == "2")
-                                {
-                                    roundRobo.weaponName = gameWeaponList.zapper.name;
-                                    roundRobo.weaponPower = gameWeaponList.zapper.attackPower;
-                                    roundRobo.energyCost = gameWeaponList.zapper.energyCost;
-                                }
-                                else if (weaponChoice == "3")
-                                {
-                                    roundRobo.weaponName = gameWeaponList.rayGun.name;
-                                    roundRobo.weaponPower = gameWeaponList.rayGun.attackPower;
-                                    roundRobo.energyCost = gameWeaponList.rayGun.energyCost;
-                                }
-                                else if(weaponChoice == "4")
-                                {
-                                    roundRobo.powerLevel = 100;
-                                }
-                                else
-                                {
-                                    roundRobo.weaponName = gameWeaponList.rayGun.name;
-                                    roundRobo.weaponPower = gameWeaponList.rayGun.attackPower;
-                                    roundRobo.energyCost = gameWeaponList.rayGun.energyCost;
-                                }
-                    
-                                        if (roundRobo.powerLevel >= roundRobo.energyCost)
-                                        {
-                                            roundDino.health = roundDino.health - roundRobo.weaponPower;
-                                            roundRobo.powerLevel = roundRobo.powerLevel - roundRobo.energyCost;
-                                        }
-                                        else
-                                        {
-                                            Console.WriteLine("Sorry you do not have enough energy. You lose your turn.");
-                                        }
-                    
-               Console.WriteLine($"{roundRobo.name}: HEALTH {roundRobo.health}, ENERGY {roundRobo.powerLevel}\n{roundDino.type}: HEALTH {roundDino.health}, ENERGY {roundDino.energy}\n");
 
-               attacker = roundDino.type;
-                    
+                if (attacker == roundRobo.name && roundRobo.powerLevel > 0)
+                {
+                    Console.WriteLine($"{roundRobo.name}: Select the number of the weapon you want to attack with or to restore your energy.\n1) Electric Blaster: PWR 50, ENG COST 25\n2) Zapper: PWR 20, ENG COST 10  \n3) Ray Gun: PWR 35, ENG COST 15\n4) Restore Energy: ENG +100\n");
+                    weaponChoice = Console.ReadLine();
+                    if (weaponChoice == "1")
+                    {
+                        roundRobo.weaponName = gameWeaponList.electricBlaster.name;
+                        roundRobo.weaponPower = gameWeaponList.electricBlaster.attackPower;
+                        roundRobo.energyCost = gameWeaponList.electricBlaster.energyCost;
+                    }
+                    else if (weaponChoice == "2")
+                    {
+                        roundRobo.weaponName = gameWeaponList.zapper.name;
+                        roundRobo.weaponPower = gameWeaponList.zapper.attackPower;
+                        roundRobo.energyCost = gameWeaponList.zapper.energyCost;
+                    }
+                    else if (weaponChoice == "3")
+                    {
+                        roundRobo.weaponName = gameWeaponList.rayGun.name;
+                        roundRobo.weaponPower = gameWeaponList.rayGun.attackPower;
+
+                        roundRobo.energyCost = gameWeaponList.rayGun.energyCost;
+                    }
+                    else if (weaponChoice == "4")
+                    {
+                        roundRobo.powerLevel = 100;
+                    }
+                    else
+                    {
+                        roundRobo.weaponName = gameWeaponList.rayGun.name;
+                        roundRobo.weaponPower = gameWeaponList.rayGun.attackPower;
+                        roundRobo.energyCost = gameWeaponList.rayGun.energyCost;
+                    }
+
+                    if (roundRobo.powerLevel >= roundRobo.energyCost)
+                    {
+                        roundDino.health = roundDino.health - roundRobo.weaponPower;
+                        roundRobo.powerLevel = roundRobo.powerLevel - roundRobo.energyCost;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry you do not have enough energy. You lose your turn.");
+                    }
+
+                    Console.WriteLine($"{roundRobo.name}: HEALTH {roundRobo.health}, ENERGY {roundRobo.powerLevel}\n{roundDino.type}: HEALTH {roundDino.health}, ENERGY {roundDino.energy}\n");
+
+                    attacker = roundDino.type;
+
+
+                }
+                else if (attacker == roundDino.type && roundDino.energy >= 0)
+                {
+                    Console.WriteLine($"{roundDino.type} attack by selecting an attack!\n1) Massive Chomp: PWR 75, ENG COST 50\n2) Massive Stomp: PWR 55, ENG 30\n3) Tail Swipe: PWR 30, ENG 15\n4) Restore Energy: ENG +100\n");
+                    attackChoice = Console.ReadLine();
+                    if (attackChoice == "1")
+                    {
+                        roundDino.attackPower = 75;
+                        roundDino.attackCost = 50;
+                    }
+                    else if (attackChoice == "2")
+                    {
+                        roundDino.attackPower = 55;
+                        roundDino.attackCost = 30;
+
+
 
                     }
-                    else if (attacker == roundDino.type && roundDino.energy >= 0)
+                    else if (attackChoice == "3")
                     {
-                        Console.WriteLine($"{roundDino.type} attack by selecting an attack!\n1) Massive Chomp: PWR 75, ENG COST 50\n2) Massive Stomp: PWR 55, ENG 30\n3) Tail Swipe: PWR 30, ENG 15\n4) Restore Energy: ENG +100\n");
-                        attackChoice = Console.ReadLine();
-                        if(attackChoice == "1")
-                        {
-                            roundDino.attackPower = 75;
-                            roundDino.attackCost = 50;
-                                
-                            
-                           
-                            
-                        }
-                        else if(attackChoice == "2")
-                        {
-                            roundDino.attackPower = 55;
-                            roundDino.attackCost = 30;
+                        roundDino.attackPower = 30;
+                        roundDino.attackCost = 15;
 
 
-                            
-                        }
-                        else if (attackChoice == "3")
-                        {
-                            roundDino.attackPower = 30;
-                            roundDino.attackCost = 15;
 
-
-                           
-                        }
-                         else if (attackChoice == "4")
-                        {
-                            roundDino.energy = 100;
-                        }
+                    }
+                    else if (attackChoice == "4")
+                    {
+                        roundDino.energy = 100;
+                    }
                     if (roundDino.energy >= roundDino.attackCost)
                     {
-
-
-                        roundRobo.health -=  roundDino.attackPower;
+                        roundRobo.health -= roundDino.attackPower;
                         roundDino.energy -= roundDino.attackCost;
                     }
                     else
@@ -143,7 +138,7 @@ namespace DinoVsRobo
 
                     attacker = roundRobo.name;
                     Console.WriteLine($"{roundRobo.name}: HEALTH {roundRobo.health}, ENERGY {roundRobo.powerLevel}\n{roundDino.type}: HEALTH {roundDino.health}, ENERGY {roundDino.energy}\n");
-                }
+        }
                     if (roundDino.health <= 0 && roundDino.type == battlefield.herd.raptor.type)
                     {
                         roundDino = battlefield.herd.teradactol;
@@ -153,18 +148,36 @@ namespace DinoVsRobo
                     }
                     else if(roundRobo.health <= 0 && roundRobo.name == battlefield.fleet.robo1.name)
                     {
-                     roundRobo = battlefield.fleet.robo2;
+                        roundRobo = battlefield.fleet.robo2;
 
-                     attacker = battlefield.fleet.robo2.name;
+                        attacker = battlefield.fleet.robo2.name;
 
-                    Console.WriteLine("R100 Died! Here comes R200!");
-                    Console.ReadLine();
+                        Console.WriteLine("R100 Died! Here comes R200!");
+                        Console.ReadLine();
                     
+                    }
+                    if (roundDino.health <= 0 && roundDino.type == battlefield.herd.teradactol.type)
+                    {
+                        roundDino = battlefield.herd.trex;
+                        attacker = battlefield.herd.trex.type;
+
+                        Console.WriteLine("Teradactol Died! Here comes the Teradactol!");
+                    }
+                    else if (roundRobo.health <= 0 && roundRobo.name == battlefield.fleet.robo2.name)
+                    {
+                        roundRobo = battlefield.fleet.robo3;
+
+                        attacker = battlefield.fleet.robo3.name;
+
+                        Console.WriteLine("R200 Died! Here comes R200!");
+                        Console.ReadLine();
+
+                    }
                 }
-                }
-            
         }
-
-
+            
     }
+
+
+    
 }
--- DinoVsRobo/Program.cs
+++ DinoVsRobo/Program.cs
@@ -12,27 +12,21 @@ namespace DinoVsRobo
         {
             WeaponList gameWeaponList = new WeaponList();
             Battlefield battlefield = new Battlefield();
-            DinoAttacks dinoAttacks = new DinoAttacks();
-           
-           
-            
-            string weaponChoice;
-            string attackChoice;
             string attacker;
 
-           
+
 
 
             Console.WriteLine("Round 1: R100 vs Raptor");
             Dinosaur roundDino = battlefield.herd.raptor;
             Robot roundRobo = new Robot("R100", 100);
-            
-           
+
+
             Console.ReadLine();
-            update(roundRobo.name, roundDino.type, roundRobo.health, roundDino.health, roundRobo.powerLevel, roundDino.energy);
-            
-           
-           
+            Console.WriteLine($"{roundRobo}: HEALTH {roundRobo.health}, ENERGY {roundRobo.powerLevel}\n{roundDino.type}: HEALTH {roundDino.health}, ENERGY {roundDino.energy}\n");
+
+
+
             attacker = roundRobo.name;
             AttackPhase attackPhaseStarter = new AttackPhase(roundRobo, roundDino, attacker);
             attackPhaseStarter.AttackPhaseStarter();
@@ -40,155 +34,7 @@ namespace DinoVsRobo
 
             Console.ReadLine();
 
-
-
-
-
-          
-
-           
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            void update(string roundRobo1, string roundDino1, int roboHealth, int dinoHealth, int roboEnergy, int dinoEnergy)
-            {
-
-                Console.WriteLine($"{roundRobo1}: HEALTH {roboHealth}, ENERGY {roboEnergy}\n{roundDino1}: HEALTH {dinoHealth}, ENERGY {dinoEnergy}\n");
-
-
-
-            }
-
-
-
-
-
-            //void AssignRoundDino()
-            //{
-            //    if (dinoChoice == "1")
-            //    {
-            //        roundDino = battlefield.herd.trex;
-            //    }
-            //    else if (dinoChoice == "2")
-            //    {
-            //        roundDino = battlefield.herd.teradactol;
-            //    }
-            //    else if (dinoChoice == "3")
-            //    {
-            //       roundDino = battlefield.herd.raptor;
-            //    }
-            //    else
-            //    {
-            //        roundDino = battlefield.herd.raptor;
-            //    }
-
-            //}
-
-            //void AssignRoundRobo()
-            //{
-            //    if (roboChoice == "1")
-            //    {
-            //        roundRobo = battlefield.fleet.robo1;
-            //    }
-            //    else if (roboChoice == "2")
-            //    {
-            //        roundRobo = battlefield.fleet.robo2;
-            //    }
-            //    else if (roboChoice == "3")
-            //    {
-            //        roundRobo = battlefield.fleet.robo3;
-            //    }
-            //    else
-            //    {
-            //        roundRobo = battlefield.fleet.robo3;
-            //    }
-
-            //}
-            void AssignWeapon()
-            {
-
-
-                if (weaponChoice == "1")
-                {
-                    roundRobo.weaponName = gameWeaponList.electricBlaster.name;
-                    roundRobo.weaponPower = gameWeaponList.electricBlaster.attackPower;
-                    roundRobo.energyCost = gameWeaponList.electricBlaster.energyCost;
-                }
-                else if (weaponChoice == "2")
-                {
-                    roundRobo.weaponName = gameWeaponList.zapper.name;
-                    roundRobo.weaponPower = gameWeaponList.zapper.attackPower;
-                    roundRobo.energyCost = gameWeaponList.zapper.energyCost;
-                }
-                else if (weaponChoice == "3")
-                {
-                    roundRobo.weaponName = gameWeaponList.rayGun.name;
-                    roundRobo.weaponPower = gameWeaponList.rayGun.attackPower;
-                    roundRobo.energyCost = gameWeaponList.rayGun.energyCost;
-                }
-                else
-                {
-                    roundRobo.weaponName = gameWeaponList.rayGun.name;
-                    roundRobo.weaponPower = gameWeaponList.rayGun.attackPower;
-                    roundRobo.energyCost = gameWeaponList.rayGun.energyCost;
-                }
-
-                //}
-                //void AssignAttack()
-                //{
-
-
-                //    if (weaponChoice == "1")
-                //    {
-                //        roundDino.attackName = dinoAttacks.massiveChomp.attackName;
-                //        roundDino.attackPower = dinoAttacks.massiveChomp.attackPower;
-                //    }
-                //    else if (weaponChoice == "2")
-                //    {
-                //        roundDino.attackName = dinoAttacks.massiveStomp.attackName;
-                //        roundDino.attackPower = dinoAttacks.massiveStomp.attackPower;
-                //    }
-                //    else if (weaponChoice == "3")
-                //    {
-                //        roundDino.attackName = dinoAttacks.tailSwipe.attackName;
-                //        roundDino.attackPower = dinoAttacks.tailSwipe.attackPower;
-                //    }
-                //    else
-                //    {
-                //        roundDino.attackName = dinoAttacks.tailSwipe.attackName;
-                //        roundDino.attackPower = dinoAttacks.tailSwipe.attackPower;
-                //    }
-                //}
-
-
-                //Console.WriteLine(battlefield.herd.trex.type);
-                //Console.ReadLine();
-
-
-
-
-
-
-            }
-            }
+}
+            
     }
 }
